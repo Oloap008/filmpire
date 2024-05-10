92e56a0d62b81c07c4dadbab1c09e8bd41ef13d9
Oloap008 <paolohenry008@gmail.com>
2024-05-10 10:02:01 +0800

    Removed code split

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { Suspense, lazy } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Loader from "./components/Loader";
 
-const Layout = lazy(() => import("./components/Layout"));
-const Movies = lazy(() => import("./components/Movies"));
-const MovieInformation = lazy(() => import("./components/MovieInformation"));
-const Actors = lazy(() => import("./components/Actors"));
-const Profile = lazy(() => import("./components/Profile"));
-
-// import Layout from "./components/Layout";
-// import Movies from "./components/Movies";
-// import MovieInformation from "./components/MovieInformation";
-// import Actors from "./components/Actors";
-// import Profile from "./components/Profile";
+import Layout from "./components/Layout";
+import Movies from "./components/Movies";
+import MovieInformation from "./components/MovieInformation";
+import Actors from "./components/Actors";
+import Profile from "./components/Profile";
 
 const router = createBrowserRouter([
   {
@@ -39,11 +32,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return (
-    <Suspense>
-      <RouterProvider router={router} />;
-    </Suspense>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
